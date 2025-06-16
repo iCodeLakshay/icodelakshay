a71ad82bb452f8eecf63ce8414abfa3a687b8aa8
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-16 17:15:56 +0530

    about section initialized

---
+++ src/components/About/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <div>About Me</div>
+                <div> Hello! I'm Lakshay, a passionate Full Stack Java Developer and BCA graduate (2025). I love creating elegant
+                    solutions to complex problems and building applications that provide exceptional user experiences. My
+                    approach combines technical expertise with creative problem-solving to deliver high-quality, scalable
+                    software.
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default About
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge"
 
-export function cn(...input){
-    return twMerge(clsx(input));
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
 }
