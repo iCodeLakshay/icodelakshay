9187c639646398eab909301e8febfc07f2e3a23a
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-22 08:17:23 +0530

    about updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 </h2>
                 <div className='max-w-4xl mx-auto flex-wrap mt-8'>
                     <p data-aos="fade-up" className="text-base sm:text-lg text-gray-300 text-center leading-relaxed px-4 lg:p-0">
-                        Hello! I'm Lakshay, a dedicated Full Stack Web Developer and BCA graduate (2025). I&#39;ve built and optimized <span className='bg-[#6826ce] px-1 py-1 rounded-lg'> 20+ real-world projects</span>, including full-stack applications like <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>LMS platforms</span> and <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>admin dashboards.</span> I focus on enhancing UI/UX, improving performance, and <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>delivering scalable, high-quality solutions</span> through a blend of technical skills and practical experience.
+                        Hello! I'm Lakshay, a dedicated Full Stack Web Developer and BCA graduate (2025). I&#39;ve built and optimized <span className='bg-[#6826ce] px-1 py-1 rounded-lg'> 5+ real-world projects</span>, including full-stack applications like <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>LMS platforms</span> and <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>admin dashboards.</span> I focus on enhancing UI/UX, improving performance, and <span className='bg-[#6826ce] px-1 py-1 rounded-lg'>delivering scalable, high-quality solutions</span> through a blend of technical skills and practical experience.
                     </p>
                 </div>
             </div>
